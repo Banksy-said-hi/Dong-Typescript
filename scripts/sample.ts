bf56073e5806b054642b4c54c7aaf08b4fcdc4d9
Sina <sinarahimi.main@example.com>
2022-07-26 16:19:09 +0430

    Successful request to the Chainlink Oracle

--- scripts/sample.ts
+++ scripts/sample.ts
@@ -10,16 +10,30 @@ async function main() {
     const accounts = await ethers.getSigners();
     const address = accounts[0].address;
     const tx = await accounts[0].getBalance();
+    console.log("Done");
     const balance = ethers.utils.formatEther(tx);
     console.log(`Our account is ${address} with the balance of ${balance} ETH`);
 
     console.log("=============================================");
 
-    console.log("Trying to deploy a contract")
+    console.log("Trying to deploy a contract!")
     const Dong = await ethers.getContractFactory("Dong");
     const instance = await Dong.deploy(accounts[0].address, 20, 6, "Kami");
     await instance.deployed();
-    console.log(`The smart contract is deployed on ${instance.address}`);
+    console.log("Done");
+    const receipt = instance.deployTransaction
+    const cost = receipt.gasPrice?.toString();
+    console.log(`The smart contract is deployed on ${instance.address} with the cost of ${cost} WEI`);
+
+    console.log("=============================================");
+
+    console.log("Trying to get the price of ETH from Chainlink!");
+    const tx1 = await instance.getLatestPrice();
+    const response = tx1.toNumber().toFixed(4);
+    console.log("Done");
+    console.log(`Ethereum price is currently $${response}`)
+
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
