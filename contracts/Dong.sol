9b2684037d64b63d6b986db54c3fa1b898927a08
Sina <sinarahimi.main@example.com>
2022-08-03 15:25:11 +0430

    Replaced ETH priceFeed contract with MATIC, needs more working!

--- contracts/Dong.sol
+++ contracts/Dong.sol
@@ -6,18 +6,16 @@ import "hardhat/console.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 // TODO:
-// (1) Change the chainlink pricefeed contract to be able to retrieve the Polygon price
-// (2) Modify the script to make it calculate the dong correctly
+// Try to fix the Chainlink interaction, it returns "0" as the current MATIC price
+// It may be relevant to the MATIC balance of the caller contract or msg.sender
+// Ask Chainlink specialists in your Telegram group
 
 contract Dong {
     AggregatorV3Interface internal priceFeed;
 
-    int256 public response;
-    uint8 public decimals;
-    uint256 public ethPrice;
+    uint256 public maticPrice;
 
     uint256 public totalDollarAmount;
-    uint256 public remainingETHAmount;
     uint256 public contributors;
     uint256 public dong;
     uint256 public counter;
@@ -25,7 +23,6 @@ contract Dong {
     string public beneficiaryName;
 
     address public beneficiary;
-    // address public creator;
 
     bool public finished;
 
@@ -36,65 +33,65 @@ contract Dong {
         address _beneficiary,
         uint256 _totalDollarAmount,
         uint256 _contributors,
-        string memory _name
+        string memory _beneficiaryName
     ) {
         priceFeed = AggregatorV3Interface(
-            0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+            0x7794ee502922e2b723432DDD852B3C30A911F021
         );
-
-        getLatestPrice();
-
-        // remainingETHAmount = _totalDollarAmount / ethPrice;
-        // dong = remainingETHAmount / contributors;
-
-        // creator = msg.sender;
         beneficiary = _beneficiary;
         totalDollarAmount = _totalDollarAmount;
         contributors = _contributors;
-        beneficiaryName = _name;
+        beneficiaryName = _beneficiaryName;
     }
 
-    function getLatestPrice() private returns (int256) {
+    function getLatestPrice() public returns (int256) {
         (
             ,
             /*uint80 roundID*/
-            response, /*uint startedAt*/ /*uint timeStamp*/ /*uint80 answeredInRound*/
+            int256 response, /*uint startedAt*/ /*uint timeStamp*/ /*uint80 answeredInRound*/
             ,
             ,
 
         ) = priceFeed.latestRoundData();
-        calculateETHPrice(response);
-        return (response);
+        return response;
     }
 
     function calculateETHPrice(int256 _price) private {
-        ethPrice = uint256(_price) * 10000000000;
+        maticPrice = uint256(_price) * 10000000000;
+    }
+
+    function calculateDong() public returns (uint256) {
+        getLatestPrice();
+        dong = totalDollarAmount / maticPrice;
+        return dong;
     }
 
     function payDong(string calldata _name) public payable {
+        // updateDong();
+
         require(msg.value >= dong, "msg.value must be at least equal to dong");
         require(finished == false, "The process has already been finished");
 
         uint256 reversal;
         uint256 net;
 
-        if (msg.value <= remainingETHAmount) {
-            reversal = msg.value % dong;
+        if (msg.value <= (totalDollarAmount / maticPrice)) {
+            reversal = msg.value % (dong * maticPrice);
             net = msg.value - reversal;
         } else {
-            reversal = msg.value - remainingETHAmount;
-            net = remainingETHAmount;
+            reversal = msg.value - (totalDollarAmount / maticPrice);
+            net = totalDollarAmount;
         }
 
-        remainingETHAmount -= net;
+        totalDollarAmount -= net;
         payment[msg.sender] += net;
 
-        counter += 1;
         names[counter] = _name;
+        counter += 1;
 
         payable(msg.sender).transfer(reversal);
 
-        if (remainingETHAmount == 0) {
+        if (totalDollarAmount == 0) {
             payable(beneficiary).transfer(address(this).balance);
             finished = true;
         }
--- scripts/sample.ts
+++ scripts/sample.ts
@@ -4,45 +4,52 @@ async function main() {
     console.log("=============================================");
 
     console.log(`Trying to connect to the ${network.name} network`);
-    console.log("Done");
+    console.log("Done Done");
 
     console.log("=============================================");
 
     console.log("Retrieving our account!");
+    console.log("Working ...");
     const accounts = await ethers.getSigners();
     const address = accounts[0].address;
     const tx = await accounts[0].getBalance();
-    console.log("Done");
+    console.log("Done! Yohooo");
     const balance = ethers.utils.formatEther(tx).slice(0, 6);
     console.log(`Our account is ${address} with the balance of ${balance} ETH`);
 
     console.log("=============================================");
 
-    console.log("Trying to deploy a contract!")
+    console.log("Trying to deploy a contract!");
+    console.log("Working ...");
     const Dong = await ethers.getContractFactory("Dong");
     const instance = await Dong.deploy(accounts[0].address, 20, 200, "Kami");
     await instance.deployed();
-    console.log("Done");
+    console.log("Done!");
     const receipt = instance.deployTransaction
     const cost = receipt.gasPrice?.toString();
     console.log(`The smart contract is deployed on ${instance.address} with the cost of ${cost} WEI`);
 
     console.log("=============================================");
 
-    console.log("Trying to get ETH price from Chainlink Oracle");
-    console.log("Done");
-    const tx1 = await instance.ethPrice();
-    const response1 = ethers.utils.formatEther(tx1);
-    console.log(`The ETH is $${response1}`);
+    console.log("Trying to get the Matic price from the Chainlink Oracle");
+    console.log("Working ...");
+    const tx1 = await instance.getLatestPrice();
+    console.log(ethers.utils.formatEther(tx1.value));
+    // const tx2 = await instance.maticPrice();
+    // const response1 = ethers.utils.formatEther(tx2);
+    // console.log("Done!");
+    // console.log(`The Matic price currently is $${response1}`);
 
-    // const tx1 = await instance.ethPrice();
-    // const response1 = ethers.utils.formatEther(tx1);
-    // console.log(`The ETH price is equal to ${response1}`);
-    // const tx2 = await instance.dong();
-    // const response2 = tx2.toString()
-    // console.log(`Dong is ${response2}`);
+    console.log("=============================================");
 
+    // console.log("Trying to get the dong amount");
+    // console.log("Working ...");
+    // const tx3 = await instance.calculateDong();
+    // const response2 = await tx3.wait();
+    // console.log(response2);
+    // console.log("Done!");
 
+ 
     // console.log("=============================================");
 
     // console.log("Trying to get the price of the ETH from Chainlink!");
